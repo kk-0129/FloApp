2f67d0a9f69c5fffe4baac5c894d449828df41f2
Keven Kearney <keven.kearney@eng.it>
2025-04-08 17:42:35 +0200

    Updated package dependencies to GitHub URLs

--- Package.swift
+++ Package.swift
@@ -14,14 +14,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../Flo2D")
+        .package(url:"https://github.com/kk-0129/Flo2D.git", from: "1.0.0"),
+        //.package(path: "../Flo2D")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "FloApp",
-            dependencies: ["Flo2D"]),
+            dependencies: [
+                .product(name: "Flo2D", package:"Flo2D")
+            ]),
     ]
 )
